8e1b5c72d9f1d1880fed34097b3bf369698d27e9
Victor Vasquez <daxos10.0@gmail.com>
2022-01-27 22:50:31 -0500

    despliegue crud

--- usuarios_app/controladores/controlador_usuarios.py
+++ usuarios_app/controladores/controlador_usuarios.py
@@ -16,9 +16,42 @@ def creaUsuario():
         "first_name" : request.form["first_name"],
         "last_name" : request.form["last_name"],
         "email" : request.form["email"]        
-    }
-    
-    resultado = Usuario.creaNuevoUsuario( nuevoUsuario )
-    print(resultado)
-
+    }    
+    Usuario.creaNuevoUsuario( nuevoUsuario )
     return redirect("/")
+
+@app.route( '/showUser/<int:id>', methods=['GET'] )
+def muestraUsuario(id):    
+    miUsuario = Usuario.buscaUsuario(id)
+    return render_template("show_user.html", usuario=miUsuario)
+
+@app.route( '/editUser/<int:id>', methods=["GET"] )
+def editarUsuario( id ):
+    """
+    usuarioAEditar = {
+        "id" : userID
+    }
+    """
+    usuarioAeditar = Usuario.buscaUsuario( id )
+    return render_template( "editarUsuario.html", usuario=usuarioAeditar)
+
+@app.route( '/updateUser/<int:id>', methods=["POST"] )
+def actualizarUsuario( id ):
+    usuarioAEditar = {
+        "id" : id,
+        "first_name" : request.form["first_name"],
+        "last_name" : request.form["last_name"],
+        "email" : request.form["email"]
+    }
+    Usuario.updateUsuario( usuarioAEditar )
+    
+    return redirect( '/' )
+
+@app.route( '/removeUser/<int:id>', methods=["POST"] )
+def eliminarUsuario( id ):
+    usuarioAEliminar = {
+        "id": id
+    }
+    Usuario.eliminarUsuario( usuarioAEliminar )
+
+    return redirect( '/' )
--- usuarios_app/modelos/Usuario.py
+++ usuarios_app/modelos/Usuario.py
@@ -19,9 +19,31 @@ class Usuario():
         return listaUsuarios
     
     @classmethod
-    def creaNuevoUsuario(cls, dataUsuario):
+    def creaNuevoUsuario(cls, dataUsuario): #no puedo enviar un objeto porque estaría incompleto, sino que le mando un dic con los datos que tengo
         query = "INSERT INTO users(first_name, last_name, email) VALUES(%(first_name)s, %(last_name)s, %(email)s);"        
         resultado = connectToMySQL( "users" ).query_db( query, dataUsuario )
+        return resultado # Devuelve un ID
+    
+    @classmethod
+    def buscaUsuario(cls, userID): # Devuelve un objeto Usuario
+        dataUsuario = {
+            'id' : userID
+        }
+        query = "SELECT * FROM users WHERE id = %(id)s;"        
+        resultado = connectToMySQL( "users" ).query_db( query, dataUsuario )
+        usuarioEncontrado = Usuario(resultado[0]["id"], resultado[0]["first_name"], resultado[0]["last_name"], resultado[0]["email"], resultado[0]["created_at"], resultado[0]["updated_at"])
+        return usuarioEncontrado
+    
+    @classmethod
+    def updateUsuario(cls, usuarioAeditar):
+        query = "UPDATE users SET first_name = %(first_name)s, last_name = %(last_name)s, email = %(email)s WHERE id = %(id)s;"
+        resultado = connectToMySQL( "users" ).query_db( query, usuarioAeditar )
+        return resultado
+
+    @classmethod
+    def eliminarUsuario(cls, usuarioAeliminar):
+        query = "DELETE FROM users WHERE id = %(id)s;"
+        resultado = connectToMySQL( "users" ).query_db( query, usuarioAeliminar )
         return resultado
 
 
